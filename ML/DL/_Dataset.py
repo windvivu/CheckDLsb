cec016bf77af9f8460189f0c19d8f75d591e46cd
windvivu <minhquan83@gmail.com>
2025-01-08 21:29:30 +0700

    2

--- ML/DL/_Dataset.py
+++ ML/DL/_Dataset.py
@@ -157,10 +157,59 @@ class SimpleCNN(nn.Module):
         x = self.fc3(x)
 
         return x
+	
+class SimpleCNNsb(nn.Module):
+    def __init__(self, num_classes=3):
+        super().__init__()
+        self.conv1 = self.make_block(in_channels=1, out_channels=8)
+        self.conv2 = self.make_block(in_channels=8, out_channels=16)
+        self.conv3 = self.make_block(in_channels=16, out_channels=32)
+        self.conv4 = self.make_block(in_channels=32, out_channels=64)
+
+        self.fc1 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=64, out_features=512),
+            nn.LeakyReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=512, out_features=1024),
+            nn.LeakyReLU()
+        )
+        self.fc3 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=1024, out_features=num_classes),
+        )
+
+    def make_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding="same"),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding="same"),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+
+
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = x.view(x.shape[0], -1) # flatten 4d to 2d
+
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
 
 if __name__ == "__main__":
 	
-	type_run = 0 # 0: image, 1: sb  #################
+	type_run = 1 # 0: image, 1: sb  #################
 	
 	batch_size = 16
 	num_epochs = 100
@@ -199,7 +248,9 @@ if __name__ == "__main__":
 		
 		train_dataloader = DataLoader(trainsetsb, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
 		test_dataloader = DataLoader(testsetsb, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=False)
-	
+
+		model = SimpleCNNsb().to(device)
+
 	else:
 		
 		trainset = RefDataset(root=os.path.join(_dir, "_no_use"))
