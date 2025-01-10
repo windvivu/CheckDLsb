27d2316eaf835e84ad33ad472e279a237d20fcc1
windvivu <minhquan83@gmail.com>
2025-01-10 15:30:27 +0700

    simplify model SimpleCNNsb

--- ML/DL/_Dataset.py
+++ ML/DL/_Dataset.py
@@ -161,51 +161,38 @@ class SimpleCNN(nn.Module):
 class SimpleCNNsb(nn.Module):
     def __init__(self, num_classes=3):
         super().__init__()
-        self.conv1 = self.make_block(in_channels=1, out_channels=8)
-        self.conv2 = self.make_block(in_channels=8, out_channels=16)
-        self.conv3 = self.make_block(in_channels=16, out_channels=32)
-        self.conv4 = self.make_block(in_channels=32, out_channels=64)
-
-        self.fc1 = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features=64, out_features=512),
-            nn.LeakyReLU()
-        )
-        self.fc2 = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features=512, out_features=1024),
-            nn.LeakyReLU()
-        )
-        self.fc3 = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features=1024, out_features=num_classes),
+        # First conv block
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
         )
 
-    def make_block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding="same"),
-            nn.BatchNorm2d(num_features=out_channels),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding="same"),
-            nn.BatchNorm2d(num_features=out_channels),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2)
+		# Second conv block
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+		# Fully connected layers
+        self.fc = nn.Sequential(
+            nn.Dropout(0.3),
+            nn.Linear(32 * 4 * 4, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, num_classes)
         )
 
 
     def forward(self,x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-
-        x = x.view(x.shape[0], -1) # flatten 4d to 2d
-
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.fc3(x)
-
-        return x
+       x = self.conv1(x)
+       x = self.conv2(x)
+       x = x.view(x.size(0), -1)  # Flatten
+       x = self.fc(x)
+       return x
 
 if __name__ == "__main__":
 	
