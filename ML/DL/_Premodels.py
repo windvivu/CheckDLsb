f4ad13208b35dae8e3ab0b50c56ba85d447903b6
windvivu <minhquan83@gmail.com>
2025-01-14 15:09:55 +0700

    add pretrain models

---
+++ ML/DL/_Premodels.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class Resnet18sb(nn.Module):
+    def __init__(self, num_classes=3):
+        super().__init__()
+        
+        self.ver = 'res18sb'
+        
+        # Load pretrained ResNet18
+        self.model = models.resnet18(pretrained=False)
+        
+        # Modify first conv layer for grayscale input (1 channel)
+        self.model.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        
+        # Modify final fully connected layer for your number of classes
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+# save model
+def savecheckpoint(model, info:dict, filename):
+    checkpoint = {
+      "model": model,
+      "accu": info["bestacu"],
+      "info": info
+	}
+    torch.save(checkpoint, filename)
