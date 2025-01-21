79f13c3415e097104008beddef9d7c73d4ea944d
windvivu <minhquan83@gmail.com>
2025-01-21 07:04:32 +0700

    4

--- ML/DL/trainning.py
+++ ML/DL/trainning.py
@@ -42,13 +42,21 @@ class EarlyStopping:
             self.best_loss = val_loss
             self.counter = 0
         return False
+
+def savecheckpoint_textfile(model, info:dict, path_no_ext:str):
+	savecheckpoint(model, info, path_no_ext + ".chk")
+	with open(path_no_ext + ".txt", "w") as f:
+		f.write('ver: ' + info["ver"] + '\n')
+		f.write('turn: ' + info["dtsturned"] + '\n')
+		f.write('accu: ' + str(info["accu"]) + '\n')
+		f.write('loss: ' + str(info["loss"]) + '\n')
+		f.write('epoch: ' + str(_epoch + epoch))
 	
 reuse_sbdtset = True
-test_phase = True
+saveby = 'accu' # loss, f1
 turndtset = ''
-if test_phase:
-	sysboltest = "NEO/USDT"
-	tftest = "4h"
+sysboltest = "NEO/USDT"
+tftest = "4h"
 
 batch_size = 32
 num_epochs = 100000
@@ -66,25 +74,22 @@ if torch.cuda.is_available():
 else:
 	device = torch.device("cpu")
 
-# kiểm tra xem "trainsetsb.pth" đã tồn tại chưa, sau đo save hoặc load file với torch
+# kiểm tra xem "trainsetsb.pth" đã tồn tại chưa
 if os.path.exists(os.path.join(_dir, "_no_use/trainsetsb.pth")) and reuse_sbdtset:
 	trainsetsb = torch.load(os.path.join(_dir, "_no_use/trainsetsb.pth"), weights_only=False)
 else:
-	# tạo trainsetsb	
-	trainsetsb = SbDataset(root=_dir2, datafolder="data/BNfuture", symbol="NEO/USDT", timeframe="4h", listIndi=list2)
-	# save trainsetsb to file by torch
-	torch.save(trainsetsb, os.path.join(_dir, "_no_use/trainsetsb.pth"))
+	print('Err: No trainset file found!')
+	exit()
 
-testsetsb = None
-if test_phase:
-	# kiểm tra xem "testsetsb.pth" đã tồn tại chưa, sau đo save hoặc load file với torch
-	if os.path.exists(os.path.join(_dir, "_no_use/testsetsb.pth")) and reuse_sbdtset:
-		testsetsb = torch.load(os.path.join(_dir, "_no_use/testsetsb.pth"), weights_only=False)
-	else:
-		# tạo testsetsb	
-		testsetsb = SbDataset(root=_dir2, datafolder="data/BNfuture", symbol=sysboltest, timeframe=tftest, listIndi=list2, train=False)
-		# save testsetsb to file by torch
-		torch.save(testsetsb, os.path.join(_dir, "_no_use/testsetsb.pth"))
+
+# kiểm tra xem "testsetsb.pth" đã tồn tại chưa, sau đo save hoặc load file với torch
+if os.path.exists(os.path.join(_dir, "_no_use/testsetsb.pth")) and reuse_sbdtset:
+	testsetsb = torch.load(os.path.join(_dir, "_no_use/testsetsb.pth"), weights_only=False)
+else:
+	# tạo testsetsb	
+	testsetsb = SbDataset(root=_dir2, datafolder="data/BNfuture", symbol=sysboltest, timeframe=tftest, listIndi=list2, train=False)
+	# save testsetsb to file by torch
+	torch.save(testsetsb, os.path.join(_dir, "_no_use/testsetsb.pth"))
 
 if turndtset == 'up':
 	trainsetsb.TURN2UP()
@@ -96,9 +101,8 @@ elif turndtset == 'none':
 	trainsetsb.TURN2NONE()
 	if testsetsb is not None: testsetsb.TURN2NONE()
 
-if test_phase:
-	if trainsetsb.turned != testsetsb.turned:
-		raise ValueError("Trainset and testset must be turned the same way")
+if trainsetsb.turned != testsetsb.turned:
+	raise ValueError("Trainset and testset must be turned the same way")
 
 class_weights = trainsetsb.calculate_class_distribution(printOut=False).to(device)
 weights = []
@@ -113,14 +117,15 @@ replacement=True
 )
 # with sampler,  shuffle must be False
 train_dataloader = DataLoader(trainsetsb, batch_size=batch_size, sampler=sampler, shuffle=False, num_workers=0, drop_last=False)
-if test_phase: test_dataloader = DataLoader(testsetsb, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=False)
+test_dataloader = DataLoader(testsetsb, batch_size=batch_size, shuffle=False, num_workers=0, drop_last=False)
 
 # checl old model file exist
 checkpoint = None
-if os.path.exists(os.path.join(_dir, "_no_use/bestcheckpoint" + turndtset + ".chk")):
-	checkpoint = torch.load(os.path.join(_dir, "_no_use/bestcheckpoint" + turndtset + ".chk"), weights_only=False, map_location=device)
+if os.path.exists(os.path.join(_dir, f"_no_use/best{saveby}_checkpoint{turndtset}.chk")):
+	checkpoint = torch.load(os.path.join(_dir, f"_no_use/best{saveby}_checkpoint{turndtset}.chk"), weights_only=False, map_location=device)
 	model = checkpoint["model"]
 	bestaccu = checkpoint["accu"]
+	bestloss = checkpoint["loss"]
 	_epoch = checkpoint["info"]["epoch"]
 else:
 	if trainsetsb.turned == '':
@@ -128,6 +133,7 @@ else:
 	else:
 		model = SimpleCNNsb(num_classes=2).to(device)
 	bestaccu = 0
+	bestloss = 100
 	_epoch = 0
 v = 'sb0' # make sure same version of model loaded from file: sb0, sb1k55, sb1k77, sb1k75
 if checkpoint is not None:
@@ -155,11 +161,8 @@ scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
 
 early_stopping = EarlyStopping()
 
-print('Begin training', end='')
-if test_phase:
-	print(' and testing...')
-else:
-	print(' only...')
+print('Begin training...')
+
 trainsetsb.calculate_class_distribution(printOut=True)
 print('Class turning: [', trainsetsb.turned,']')
 for epoch in range(num_epochs):
@@ -180,45 +183,57 @@ for epoch in range(num_epochs):
 	trainlossItem = trainloss.item()
 	print(' End epoch', epoch+1,'loss value of training:', trainlossItem)
 	
-	if test_phase:
-		model.eval()
-		all_predictions = []
-		all_labels = []
-		for images, labels_test in tqdm(test_dataloader, desc=" Testing"):
-			images = images.to(device)
-			labels_test = labels_test.to(device)
-			
-			all_labels.extend(labels_test)
-			with torch.no_grad():
-				outputs_test = model(images) # forward
-			max_to_label = torch.argmax(outputs_test, dim=1)
-			testloss = criterion(outputs_test, labels_test) # loss
-			all_predictions.extend(max_to_label)
-		accu = (torch.tensor(all_predictions) == torch.tensor(all_labels)).sum().item()/len(all_labels)
-		testlossItem = testloss.item()
+	model.eval()
+	all_predictions = []
+	all_labels = []
+	for images, labels_test in tqdm(test_dataloader, desc=" Testing"):
+		images = images.to(device)
+		labels_test = labels_test.to(device)
 		
-		scheduler.step(testlossItem)
+		all_labels.extend(labels_test)
+		with torch.no_grad():
+			outputs_test = model(images) # forward
+		max_to_label = torch.argmax(outputs_test, dim=1)
+		testloss = criterion(outputs_test, labels_test) # loss
+		all_predictions.extend(max_to_label)
+	accu = (torch.tensor(all_predictions) == torch.tensor(all_labels)).sum().item()/len(all_labels)
+	testlossItem = testloss.item()
 
-		print('Test accuracy:', accu)
-		print('Loss value of testing:', testlossItem)
-		print(' End epoch', epoch+1, end=' - ')
-		if early_stopping(testlossItem):
-			print("Early stopping !!!!!!!!!!!!!")
-			break
+	scheduler.step(testlossItem)
+	current_lr = optimizer.param_groups[0]['lr']
+	print(f'Current learning rate: {current_lr}')
+	print('Test accuracy:', accu)
+	print('Loss value of testing:', testlossItem)
+	print('== End epoch', epoch+1, end=' - ')
 	
-	if accu > bestaccu:
-		bestaccu = accu
-		info = {
-			'ver': model.ver, 
-			'indi': list2, 
-			'accu': bestaccu,
-			'epoch': _epoch + epoch,
-			'classes': trainsetsb.class_to_idx2,
-			'dtsturned': trainsetsb.turned
-		}
-		savecheckpoint(model, info, os.path.join(_dir, "_no_use/bestcheckpoint" + turndtset + ".chk"))
-		with open(os.path.join(_dir, "_no_use/bestcheckpoint" + turndtset + ".txt"), "w") as f:
-			f.write(model.ver + '\n')
-			f.write( trainsetsb.turned + '\n')
-			f.write(str(bestaccu) + '\n')
-			f.write(str(_epoch + epoch))
+	if saveby == 'accu':
+		if accu > bestaccu:
+			bestaccu = accu
+			info = {
+				'ver': model.ver, 
+				'indi': list2, 
+				'accu': bestaccu,
+				'loss': testlossItem,
+				'epoch': _epoch + epoch,
+				'classes': trainsetsb.class_to_idx2,
+				'dtsturned': trainsetsb.turned
+			}
+			savecheckpoint_textfile(model, info, os.path.join(_dir, f"_no_use/best{saveby}_checkpoint{turndtset}"))
+	elif saveby == 'loss':
+		if testlossItem < bestloss:
+			bestloss = testlossItem
+			info = {
+				'ver': model.ver, 
+				'indi': list2, 
+				'accu': accu,
+				'loss': bestloss,
+				'epoch': _epoch + epoch,
+				'classes': trainsetsb.class_to_idx2,
+				'dtsturned': trainsetsb.turned
+			}
+			savecheckpoint_textfile(model, info, os.path.join(_dir, f"_no_use/best{saveby}_checkpoint{turndtset}"))
+			
+	
+	if early_stopping(testlossItem):
+		print("Early stopping !!!!!!!!!!!!!")
+		break
